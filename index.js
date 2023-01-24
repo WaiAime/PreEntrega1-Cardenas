72a07aa7d1c16b430af1e6f9a76d0a000a21e15e
Waira Cárdenas <waira.cardenas@hotmail.com>
2023-01-24 00:09:57 -0300

    fin primer pre entrega

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ let saludo = "Bienvenido " + nombreAsignado;
 console.log(saludo);
 alert(saludo)
 
-
-
 let salir=false;
-
 function sumar(numero1,numero2){
     return numero1+numero2;
 }
@@ -24,9 +21,10 @@ function modulo(numero1,numero2){
     return numero1%numero2;
 }
 
-
-
 while(!salir){
+    let numero1=parseInt(prompt("Ingresa el 1er Nro"));
+    let numero2=parseInt(prompt("Ingresa el 2do Nro"));
+
     let opcion=parseInt(prompt("Elegí una opción:"+
     "1:Sumar "+
     "2:Restar "+
@@ -35,45 +33,42 @@ while(!salir){
     "5:Porcentaje "+
     "6:SALIR"));
     
-    let numero1=parseInt(prompt("Ingresa el 1er Nro"));
-    let numero2=parseInt(prompt("Ingresa el 2do Nro"));
     let resultado=0;
     
     switch (opcion){
         case 1:
             resultado=sumar(numero1,numero2);
             break;
-            case 2:
-                resultado=restar(numero1,numero2);
+        case 2:
+            resultado=restar(numero1,numero2);
             break;
-            case 3:
-                if(numero2==0){
-                    alert("No se puede dividir entre cero");
-                }
-                else{
-                    (resultado=dividir(numero1,numero2));
-                }
+        case 3:
+            if(numero2==0){
+                alert("No se puede dividir entre cero");
+            }
+            else{
+                (resultado=dividir(numero1,numero2));
+            }
             break;    
-            case 4:
-                resultado=multiplicar(numero1,numero2);
-                break;
-            case 5:
-                resultado=porcentaje(numero1,numero2);
+        case 4:
+            resultado=multiplicar(numero1,numero2);
             break;
-            case 6:
-                resultado= salir
-                alert("Fin de los calculos");
-                salir=true;
+        case 5:
+            resultado=porcentaje(numero1,numero2);
+        break;
+        case 6:
+            alert("Has elegido salir");
+            salir=true;
             break;
-            default:
-                alert("Solo numeros entre 1 y 6")
+        default:
+            alert("Solo numeros entre 1 y 6")
     }
     if(opcion>=1 && opcion<=2 || opcion>=4 && opcion<=5 ){
         alert("Resultado: "+resultado);
-    }else if(opcion==3 && numero2===0){
-        alert("Intente nuevamente gracias")
+    } else if(opcion>=1 && opcion<=5 ){
+        alert("El resultado es: "+resultado);
     }else{
-        alert("Fin de los calculos");
+        alert("Por favor contesta la breve encuesta");
     }
 
 };
